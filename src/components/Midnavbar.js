44b3c83e718d7ff18481d20d2c1f100d656a0237
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-03-24 01:26:34 +0800

    Add parallax to SectionThree

--- src/components/Midnavbar.js
+++ src/components/Midnavbar.js
@@ -1,13 +1,16 @@
 import React, { useRef, useState } from 'react'
 import {motion} from 'framer-motion'
+import { useInView } from 'react-intersection-observer';
+
 
 
 function Midnavbar() {
+  const { ref:stickyNavRef, inView:showIsvisible } = useInView();
 
   return (
-    <div className=' sticky top-0 midnav text-md text-xl text-white'
+    <div ref={stickyNavRef} className={`${showIsvisible ? 'sticky top-0' : ''} midnav text-md text-xl text-white`}
     >
-        <nav className='flex justify-around bg-white/5 poppins '>
+        <nav className='flex justify-around bg-gray-800 poppins '>
             <span className='p-4'>About</span>
             <span className='p-4'>Mastery</span>
             <span className='p-4'>Timeline</span>
--- src/components/SectionThree.js
+++ src/components/SectionThree.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useInView } from 'react-intersection-observer';
+
 
 function SectionThree() {
+const { ref:stickyRef, inView:showIsvisible } = useInView();
   return (
-    <div className='flex text-xl items-center text-white'>
-        <div className="headings flex-[50%] flex flex-col justify-center items-baseline h-[90vh]">
+    <div className='flex text-xl text-white h-[130vh]'>
+        <div ref={stickyRef} className={`${showIsvisible ? 'sticky bottom-0' : ''} headings flex-[50%] flex flex-col justify-center items-baseline h-[90vh]`}>
         <h1 className='pl-20 text-4xl leading-8 pb-5 nunito font-semibold'>Presently conquering fluency in MERN stack</h1>
         <p className='pl-20 poppins leading-5 italic'>consist with four powerful and modern technologies</p>
         </div>
