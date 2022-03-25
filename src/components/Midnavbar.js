855a7f147b9b2f7fc01a712ff7e023075c9431da
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-03-26 00:01:05 +0800

    Add midnav mobile-view functionality

--- src/components/Midnavbar.js
+++ src/components/Midnavbar.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState,useCallback,useEffect } from 'react'
 import {motion} from 'framer-motion'
 import { useInView } from 'react-intersection-observer';
-import {FiMenu} from 'react-icons/fi'
+import {FiMenu,FiX} from 'react-icons/fi'
 import logo from '../images/logo.png'
 
 
@@ -9,6 +9,8 @@ import logo from '../images/logo.png'
 
 function Midnavbar() {
   const { ref:stickyNavRef, inView:showIsvisible } = useInView();
+  const [toggle, setToggle] = useState(false)
+
 
 
   return (
@@ -18,9 +20,11 @@ function Midnavbar() {
 
         >
             <img src={logo} alt="Logo" className="h-12 ml-20"/>
-            <FiMenu className='block md:hidden scale-150 mr-12 '/>
+            <FiMenu onClick={() =>setToggle(true)} className='block md:hidden scale-150 mr-12 '/>
         </div>
-        <nav className='md:flex md:justify-around bg-gray-800 w-full poppins hidden '>
+        <nav className={`${toggle ? 'translate-x-0': ''} flex md:justify-around md:flex-row md:h-20 md:static bg-gray-800 w-screen fixed top-0 h-[50vh] translate-x-[100vw]  poppins flex-col items-center justify-end`} >
+
+            <FiX onClick={() =>setToggle(false)} className="block md:hidden cursor-pointer" />
             <span className='p-4'>About</span>
             <span className='p-4'>Mastery</span>
             <span className='p-4'>Timeline</span>
