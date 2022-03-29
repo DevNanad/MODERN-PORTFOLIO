7ecd84d18e39aaa331bfe1e76e306c9248718cdb
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-03-30 01:27:56 +0800

    Add Logo to Midnav

--- src/components/Midnavbar.js
+++ src/components/Midnavbar.js
@@ -3,6 +3,7 @@ import {motion} from 'framer-motion'
 import { useInView } from 'react-intersection-observer';
 import {FiMenu,FiX} from 'react-icons/fi'
 import logo from '../images/logo.png'
+import logowhite from '../images/logowhite.png'
 import { Link,animateScroll as scroll  } from "react-scroll";
 
 
@@ -17,22 +18,24 @@ function Midnavbar() {
   return (
     <div ref={stickyNavRef} className={`${showIsvisible ? 'sticky top-0' : ''} midnav text-md text-xl text-white flex z-40 ease-in bg-[#292C35] shadow-lg`}
     >   
-        <div className={`logo py-3 w-full flex justify-between items-center md:hidden`}
+        <div className={`logo py-3 w-full flex justify-between items-center md:justify-center md:w-3/12`}
 
         >
-            <img src={logo} alt="Logo" onClick={() => scroll.scrollToTop()} className="h-12 ml-20"/>
-            <div onClick={() => setToggle(true)} className="btn-open p-2 mr-12">
-            <FiMenu className='text-blue-300 hover:text-blue-100 text-2xl block md:hidden scale-150 '/>
+            <img src={logo} alt="Logo" onClick={() => scroll.scrollToTop()} className="h-12 ml-20 md:ml-0"/>
+            <div onClick={() => setToggle(true)} className="btn-open p-2 mr-12 md:hidden ">
+              <FiMenu className='text-blue-300 hover:text-blue-100 text-2xl block md:hidden scale-150 '/>
             </div>
         </div>
-        <nav className={`${toggle ? 'transform-none ease-in duration-300 pb-4 md:pb-0': '-translate-y-[100vh] ease-out duration-300'} midlink flex md:justify-around md:flex-row md:h-20 md:static bg-[#718BFF] md:bg-[#292C35] w-screen fixed top-0 h-[58vh] -translate-y-[100vh] md:transform-none  poppins flex-col items-center justify-end shadow-md z-50 rounded-lg`} >
+        <nav className={`${toggle ? 'transform-none ease-in duration-300 pb-4 md:pb-0': '-translate-y-[100vh] ease-out duration-300'} midlink flex md:justify-around md:flex-row md:h-24 md:static bg-[#718BFF] md:bg-[#292C35] w-screen fixed top-0 h-[58vh] -translate-y-[100vh] md:transform-none  poppins flex-col items-center justify-end shadow-md z-50 rounded-lg`} >
 
-            <div onClick={() =>setToggle(false)} className="btn-close block md:hidden w-full h-[8vh] text-center hover:shadow-md">
-              <FiX className=" cursor-pointer text-3xl w-full" />
+            <div onClick={() =>setToggle(false)} className="btn-close flex justify-around items-center md:hidden w-full h-[8vh] text-center hover:shadow-md">
+              {/*Logo*/}
+
+              <img src={logowhite} alt="Logo" onClick={() => scroll.scrollToTop()} className="h-12"/>
+              <FiX className=" cursor-pointer text-3xl" />
             </div>
 
             <Link 
-            onClick={() =>setToggle(false)}
             to="secHello"
             activeClass="active"
             smooth={true}
@@ -42,14 +45,13 @@ function Midnavbar() {
             className='cursor-pointer p-4 hover:shadow-md hover:rounded-lg w-full text-center flex flex-col items-center'>
               <p>About</p>
                 <span className="hidden h-3 w-3">
-                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-sky-400 opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-[#2ff3ff] opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-3 w-3 bg-[#2ff3ff]"></span>
               </span> 
             </Link>
 
             
               <Link 
-              onClick={() =>setToggle(false)}
               to="secThree" 
               activeClass="active"
               smooth={true}
@@ -59,14 +61,13 @@ function Midnavbar() {
               className='cursor-pointer p-4 hover:shadow-md hover:rounded-lg w-full text-center flex flex-col items-center'>
                 <p>Mastery</p>
                 <span className="hidden h-3 w-3">
-                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-sky-400 opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-[#2ff3ff] opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-3 w-3 bg-[#2ff3ff]"></span>
               </span> 
               </Link>
 
 
               <Link 
-              onClick={() =>setToggle(false)}
               to="secFour"
               activeClass="active"
               smooth={true}
@@ -77,8 +78,8 @@ function Midnavbar() {
               flex flex-col items-center'>
                 <p>Timeline</p>
                 <span className="hidden h-3 w-3">
-                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-sky-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-[#2ff3ff] opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-[#2ff3ff]"></span>
                 </span> 
               </Link>
 
