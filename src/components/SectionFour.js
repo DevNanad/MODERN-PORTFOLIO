cf2c27fe354d690c58ecffc9d7c298d7f2eaad03
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-04-01 22:41:49 +0800

    Clean SectionFour content

--- src/components/SectionFour.js
+++ src/components/SectionFour.js
@@ -16,41 +16,55 @@ function SectionFour() {
         <div className=" h-[150vh] pt-36 timeline md:flex">
 
           {/* Left timeline */}
-        <div className="left-timeline px-10 md:flex md:justify-around flex-col flex-[40%] items-end text-right">
+        <div className="left-timeline px-10 md:flex flex-col flex-[40%] text-right">
           
           {/* Left ONE */}
           <div  ref={yearone} className="wrapper-timeline">
-            {showIsvisible && (<motion.div className="first-content"
-            initial={{ x: "-100px"}}
-            animate={{ x: 0}}
-            transition={{ type: 'tween', stiffness: 250, duration: 1 }}
-            >
+            <div className="first-content">
               <div className="year-title rounded-lg text-center font-bold poppins md:mb-6 bg-white text-[#616060] shadow-2xl">
                 <h2 className='text-4xl pt-3'>2014</h2>
                 <h5 className='text-sm paintedblue pb-3'>Graduated Grade School</h5>
               </div>
               <p className='shadow-lg p-4 text-center'>A Government grade school for its citizen located at Zapote Las Pinas City.</p>
-            </motion.div>)}
+            </div>
+          </div>
+
+          {/* RIGHT ONE */}
+          <div  ref={junhigh} className="wrapper-timeline">
+            <div className="first-content">
+              <div className="year-title rounded-lg text-center font-bold poppins md:mb-6 bg-white text-[#616060] shadow-2xl">
+                <h2 className='text-4xl pt-3'>2018</h2>
+                <h5 className='text-sm paintedblue pb-3'>Graduated Junior High School</h5>
+              </div>
+              <p className='shadow-lg p-4 text-center'>At PAA (Palawan Adventist Academy) an intitution of Christ founded to spread gospel to everyone bough of academics, located at Tacras Narra Palawan</p>
+            </div>
           </div>
 
           {/* Left TWO */}
           <div  ref={senhighs} className="wrapper-timeline">
-            {showSenHighSIsvisible && (<motion.div className="first-content"
-            initial={{ x: "-100px"}}
-            animate={{ x: 0}}
-            transition={{ type: 'tween', stiffness: 250, duration: 1 }}
-            >
+            <div className="first-content">
               <div className="year-title rounded-lg text-center font-bold poppins md:mb-6 bg-white text-[#616060] shadow-2xl">
                 <h2 className='text-4xl pt-3'>2020 (March)</h2>
                 <h5 className='text-sm paintedblue pb-3'>Graduated Senior High School</h5>
               </div>
               <p className='shadow-lg p-4 text-center'>At NLAC (Northern luzon Adventist College) an Adventist institution which value relationship with God, located at Artacho Sison Pangasinan.</p>
-            </motion.div>)}
+            </div>
+          </div>
+
+          {/* RIGHT TWO */}
+          <div  ref={college} className="wrapper-timeline">
+            <div className="first-content">
+              <div className="year-title rounded-lg text-center font-bold poppins md:mb-6 bg-white text-[#616060] shadow-2xl">
+                <h2 className='text-4xl pt-3'>2020 (August)</h2>
+                <h5 className='text-sm paintedblue pb-3'>Started College</h5>
+              </div>
+              <p className='shadow-lg p-4 text-center'>At TCU (Taguig City University) currently taking bachelors degree of Computer Science (BSCS)</p>
+            </div>
           </div>
           
         </div>
 
-        {/* Bar timeline */}
+        {/* Bar timeline
         <div className="bar-line w-2 h-[190vh] rounded-xl bg-[#718BFF]">
           <span className='circlespan absolute -translate-x-3 rounded-full w-8 h-8 bg-inherit shadow-2xl'></span>
 
@@ -59,43 +73,8 @@ function SectionFour() {
           <span className={`${showSenHighSIsvisible ? 'showline' : ''} linespan absolute -translate-x-10 translate-y-[36.5rem] h-2 w-10 bg-[#718BFF]`}></span>
 
 
-        </div>
-
-        {/* Right timeline */}
-        <div className="right-timeline px-10 md:flex md:justify-around flex-col flex-[40%] items-end text-right">
+        </div> */}
           
-          {/* RIGHT ONE */}
-          <div  ref={junhigh} className="wrapper-timeline">
-            {showJunHighSIsvisible && (<motion.div className="first-content"
-            initial={{ x: "100px"}}
-            animate={{ x: 0}}
-            transition={{ type: 'tween', stiffness: 250, duration: 1 }}
-            >
-              <div className="year-title rounded-lg text-center font-bold poppins md:mb-6 bg-white text-[#616060] shadow-2xl">
-                <h2 className='text-4xl pt-3'>2018</h2>
-                <h5 className='text-sm paintedblue pb-3'>Graduated Junior High School</h5>
-              </div>
-              <p className='shadow-lg p-4 text-center'>At PAA (Palawan Adventist Academy) an intitution of Christ founded to spread gospel to everyone bough of academics, located at Tacras Narra Palawan</p>
-            </motion.div>)}
-          </div>
-
-          {/* RIGHT TWO */}
-          <div  ref={college} className="wrapper-timeline">
-            {showCollegeIsvisible && (<motion.div className="first-content"
-            initial={{ x: "100px"}}
-            animate={{ x: 0}}
-            transition={{ type: 'tween', stiffness: 250, duration: 1 }}
-            >
-              <div className="year-title rounded-lg text-center font-bold poppins md:mb-6 bg-white text-[#616060] shadow-2xl">
-                <h2 className='text-4xl pt-3'>2020 (August)</h2>
-                <h5 className='text-sm paintedblue pb-3'>Started College</h5>
-              </div>
-              <p className='shadow-lg p-4 text-center'>At TCU (Taguig City University) currently taking bachelors degree of Computer Science (BSCS)</p>
-            </motion.div>)}
-          </div>
-
-          
-        </div>
 
         </div>
         
