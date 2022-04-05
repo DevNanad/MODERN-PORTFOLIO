2f47c87931ba6050146759316eced5ecf6cb5667
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-04-05 17:37:59 +0800

    Fix bugs and Add All projects plain

--- src/components/SectionFive.js
+++ src/components/SectionFive.js
@@ -3,13 +3,16 @@ import proj from '../images/proj.png'
 
 function SectionFive() {
   return (
-    <div className='h-[100vh] flex flex-col md:flex-row text-white  poppins' id='secFive'>
-      
-      {/* Latest Project */}
-      <div className="contOne flex-[50%] flex flex-col justify-center px-5">
+    <div className='text-white  poppins relative top-0' id='secFive'>
 
-          
-          <h1 className='text-[3rem] mt-20 pb-10 text-center poppins font-semibold tracking-wider'>LATEST PROJECT</h1>
+      <h1 className='text-[4rem] text-center pt-28 pb-20 font-mono'>PROJECTS</h1>
+      
+      <div className="project-wrapper flex flex-col md:flex-row">
+
+        {/* Latest Project */}
+        <div className="contOne flex-[50%] flex flex-col justify-center px-5">
+
+          <h1 className='text-[2rem] pb-10 text-center poppins font-semibold tracking-wider'>LATEST PROJECT</h1>
           <div className="latest-wrap rounded-md overflow-hidden h-52  flex justify-center items-center w-full bg-[#718BFF]">
 
               <img src={proj} alt="latest-proj-image" className='w-1/2 px-5' />
@@ -26,15 +29,20 @@ function SectionFive() {
           <p className="about-project text-justify py-5 text-slate-200">
             My latest Portfolio currently under devevelopment, showcasing frontend ability.
           </p>
-          
-          
 
+        </div>
+
+        {/* All Projects */}
+        <div className="contTwo flex-[50%] flex justify-center">
+          <div class="grid grid-rows-2 grid-cols-2 grid-flow-col gap-4 w-full px-5 rounded-xl overflow-hidden">
+            <div className='bg-blue-300'>PROJECT 01</div>
+            <div className='bg-yellow-300'>PROJECT 02</div>
+            <div className='bg-red-300'> PROJECT 03</div>
+            <div className='bg-violet-300'>PROJECT 04</div>
+          </div>
+        </div>
       </div>
 
-      {/* All Projects */}
-      <div className="contTwo flex-[50%]">
-        
-      </div>
     </div>
   )
 }
