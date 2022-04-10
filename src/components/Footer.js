8018c8506102eae28553e601ecf814d78856377f
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-04-10 13:37:56 +0800

    Fix Footer responsiveness and add Social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,22 @@ function Footer() {
 
         <p className='w-3/5 py-8 text-center text-sm'>The entire menardpajares.com Web site is Copyright &copy;2022 by Menard Pajares. All Rights Reserved. The menardpajares.me site may not be copied or duplicated in whole part by any means without express prior agreement in writing or unless specifically noted on the site.</p>
 
-        <div className="objectives font-semibold flex gap-5 text-sm md:text-md md:gap-20">
-            <h3 className='flex items-center'>Clean <AiFillCheckCircle className='text-[#2ded4d]'/></h3>
-            <h3 className='flex items-center'>Responsive <AiFillCheckCircle className='text-[#2ded4d]'/></h3>
+        <div className="objectives font-semibold grid grid-cols-2 gap-4 md:gap-12 lg:gap-20 md:grid-cols-4 text-sm md:text-md">
+
+            <h3 className='flex items-center md:justify-center'>Clean <AiFillCheckCircle className='text-[#2ded4d]'/></h3>
+
             <h3 className='flex items-center'>Typography <AiFillCheckCircle className='text-[#2ded4d]'/></h3>
+
+            <h3 className='flex items-center'>Responsive <AiFillCheckCircle className='text-[#2ded4d]'/></h3>
+            
             <h3 className='flex items-center'>Appealing <AiFillCheckCircle className='text-[#2ded4d]'/></h3>
         </div>
 
         <div className="icon-wrapper flex justify-center gap-10 pt-10">
-            <AiFillTwitterCircle className='text-4xl'/>
-            <RiFacebookCircleFill className='text-4xl'/>
-            <AiFillLinkedin className='text-4xl'/>
+            <a href="https://twitter.com/Kyah_Nad" target="_blank" rel="noreferrer noopener"><AiFillTwitterCircle className='text-4xl cursor-pointer'/></a>
+            
+            <a href="https://www.facebook.com/jheeyiem" target="_blank" rel="noreferrer noopener"><RiFacebookCircleFill className='text-4xl cursor-pointer'/></a>
+            <a href="https://www.linkedin.com/in/pajares-menard-m-986a55222/" target="_blank" rel="noreferrer noopener"><AiFillLinkedin className='text-4xl cursor-pointer'/></a>
         </div>
 
     </div>
