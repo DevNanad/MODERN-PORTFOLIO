30f5178ed57eefbf78eac7f7f0b8193de8e2328a
DevNanad <74762919+DevNanad@users.noreply.github.com>
2022-12-28 19:11:15 +0800

    add reusables component for patch paragraph

--- src/pages/subpages/DashStory.js
+++ src/pages/subpages/DashStory.js
@@ -1,35 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import { useTimelineStore } from "../../store/zustand";
+import PatchParag from "./reusables/PatchParag";
 
 export default function DashStory() {
 
   const [imagePath, setImagePath] = useState("")
   const [newParagraph, setNewParagraph] = useState("")
-  const { mystoryUpload, token } = useTimelineStore((state) => state)
+  const { mystoryUpload } = useTimelineStore((state) => state)
 
 
-
-  //update paragraph in story
-  const mystoryUpdateParagraph = async (e) => {
-    e.preventDefault()
-
-    // console.log(token);
-    // console.log(newParagraph);
-
-    await fetch("http://localhost:4000/api/admin/story/63ab112d7ecbc1bae3325a2d", {
-        method: "PATCH",
-        body: JSON.stringify({
-            paragraph1: newParagraph
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-            'auth-token': token.token
-        }
-    }).then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
-  }
 
   const uploadImage = () =>{
     mystoryUpload(imagePath)
@@ -65,10 +45,7 @@ export default function DashStory() {
               />
             </div>
 
-            <button 
-            type="submit" 
-            onClick={mystoryUpdateParagraph}
-            className="bg-blue-400  py-2 px-5 rounded-full font-semibold hover:bg-blue-600">Update</button>
+            <PatchParag paraName="paragraph1" paragraph={newParagraph}/>
           </form>
 
 
